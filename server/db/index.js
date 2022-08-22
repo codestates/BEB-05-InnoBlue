9a387ed7c56c299e9189a0fa681c12c934c7d3c2
hyenne <hyeinlee601@gmail.com>
2022-08-22 10:25:37 +0900

    temporary comment out for conflict

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql2');
-const config = require('../config/config');
+// const mysql = require('mysql2');
+// const config = require('../config/config');
 
-const con = mysql.createConnection(
-  config['development']
-);
+// const con = mysql.createConnection(
+//   config['development']
+// );
 
-con.connect((err) => {
-  if (err) throw err;
-});
+// con.connect((err) => {
+//   if (err) throw err;
+// });
 
-module.exports = con;
+// module.exports = con;
