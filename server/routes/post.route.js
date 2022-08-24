01f0c076d6b4441ee83d0f8cda545a116d17fb05
bcdy19 <bcdy19@gmail.com>
2022-08-25 04:00:10 +0900

    게시글 작성 추가

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/post.controller");
 
-router.post("/postlist", controller.postlist);
+router.get("/postlist", controller.postlist);
 router.post("/writepost", controller.writepost);
-router.post("/readpost", controller.readpost);
+router.get("/readpost", controller.readpost);
 router.post("/updatepost", controller.updatepost);
 router.post("/delpost", controller.delpost);
 
