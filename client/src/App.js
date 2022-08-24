bd8cb110929e841bb5566f5d16dd96003312507c
dhjefflee <lexwoodtrans@gmail.com>
2022-08-24 10:02:05 +0900

    0824 mainpage version 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 // import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
 import MainPage from './pages/MainPage';
 import WritePage from './pages/WritePage';
-
 // import MyPage from './pages/MyPage';
 // import DetailPage from './pages/DetailPage';
-import SignUpPage from './pages/SignUpPage';
+// import SignUpPage from './pages/SignUpPage';
 // import NFTmintPage from './pages/NFTmintPage';
 // import NotFound from './pages/NotFound'; //optional
 // import Footer from './components/Footer'; //optional
 
-
 function App() {
+  
+
   return (
     <BrowserRouter>
     <div className="App">
@@ -29,7 +31,7 @@ function App() {
             <Route path = '/WritePage' element = {<WritePage />} />
             {/* <Route path = '/mypage' element = {<MyPage />} /> */}
             {/* <Route path = '/detail/:id' element = {<DetailPage />} /> */}
-            <Route path = '/signup' element = {<SignUpPage />} />
+            {/* <Route path = '/signup' element = {<SignUpPage />} /> */}
             {/* <Route path = '/nftmint' element = {<NFTmintPage />} /> */}
             {/* <Route path = '*' element = {<NotFound />} /> */}
           </Routes>
