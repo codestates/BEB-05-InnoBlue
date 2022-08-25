504ef9394cb2147234d758e28e76b5d44b818807
hyenne <hyeinlee601@gmail.com>
2022-08-25 14:37:31 +0900

    clean up

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -27,55 +27,45 @@ const postlist = async(req, res, next) => { // 게시글 전체 리스트
 }
 
 const writepost = async(req, res, next) => { // 게시글 작성
-    const body = req.body;
-
     const result = await Post.create({
-        userId: body.userId,
-        nickname: body.nickname,
-        title: body.title,
-        content: body.content,
+        userId: req.body.userId,
+        nickname: req.body.nickname,
+        title: req.body.title,
+        content: req.body.content,
     });
+
     if (!result) {
-            res.status(400).send({ data: null, message: '에러'})
-    } else {
-        // res.status(200).json({ data: result, message: '게시글 작성 완료' })
-
-        const incentive = 1;
-        const tokenContract = await new web3.eth.Contract(
-            TOKEN_CONTRACT_ABI,
-            TOKEN_CONTRACT_ADDR,
-            {from: process.env.SERVER_ADDRESS}
-        );
-        
-        
-        await User.findOne({
-            where: {
-                id: body.userId
-            }
-        }).then(async (user)=>{  
-            const data = await tokenContract.methods.transfer(user.address, incentive).encodeABI();    
-            const rawTransaction = {"to": TOKEN_CONTRACT_ADDR, "gas": 1000000, "data": data};
-
-            const signedTx = await web3.eth.accounts.signTransaction(rawTransaction, process.env.SERVER_PRIVATE_KEY);
-            const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-
-            const token_amount = await tokenContract.methods.balanceOf(user.address).call();
-            console.log(token_amount);
-            const server_token_amount = await tokenContract.methods.balanceOf(process.env.SERVER_ADDRESS).call();
-            console.log(server_token_amount);
-
-            // await User.update({token_amount:server_token_amount}, {where: {address:process.env.SERVER_ADDRESS}});
-            await User.update({token_amount:token_amount}, {where: {address:user.address}})
-            .then((result)=>{
-                res.status(201).json({
-                    message: "게시글 작성 완료! 토큰 보상이 지급되었습니다.",
-                    // data: { id: user.email, token_amount: token_amount},
-                })
-            }).catch((err)=> console.error(err));            
-        });      
+        res.status(400).send({ data: null, message: '에러'})
+        return;
     }
 
+    const incentive = 1;
+    const tokenContract = await new web3.eth.Contract(
+        TOKEN_CONTRACT_ABI,
+        TOKEN_CONTRACT_ADDR,
+        {from: process.env.SERVER_ADDRESS}
+    );
+    const user = await User.findOne({
+        where: {
+            id: req.body.userId
+        }
+    })
+    const data = await tokenContract.methods.transfer(user.address, incentive).encodeABI();    
+    const rawTransaction = {"to": TOKEN_CONTRACT_ADDR, "gas": 1000000, "data": data};
+    const signedTx = await web3.eth.accounts.signTransaction(rawTransaction, process.env.SERVER_PRIVATE_KEY);
+    await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    const token_amount = await tokenContract.methods.balanceOf(user.address).call();
+    const server_token_amount = await tokenContract.methods.balanceOf(process.env.SERVER_ADDRESS).call();
 
+    console.log(token_amount);
+    console.log(server_token_amount);
+
+    // await User.update({token_amount:server_token_amount}, {where: {address:process.env.SERVER_ADDRESS}});
+    await User.update({token_amount: token_amount}, {where: {address: user.address}});
+    res.status(201).json({
+        message: "게시글 작성 완료! 토큰 보상이 지급되었습니다.",
+        // data: { id: user.email, token_amount: token_amount},
+    })
 }
 
 const readpost = async(req, res, next) => { // 게시글 조회
