80a39696e0a63713abdd1967374de02b47e8dd9f
hyenne <hyeinlee601@gmail.com>
2022-08-25 14:01:28 +0900

    token incentive for post writing

--- server/controllers/main.controller.js
+++ server/controllers/main.controller.js
@@ -1,30 +1,10 @@
 const { User } = require('../models');
 const Web3 = require('web3');
-const {
-    NFT_CONTRACT_ADDR,
-    TOKEN_CONTRACT_ADDR,
-    NFT_CONTRACT_ABI,
-    TOKEN_CONTRACT_ABI
-} = require('../global_variables');
-
+const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
 
 exports.init_server = async () => {
-    const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
     const accounts = await web3.eth.getAccounts();
     const address = accounts[0];
-
-    const nftContract = await new web3.eth.Contract(
-        NFT_CONTRACT_ABI,
-        NFT_CONTRACT_ADDR
-    );
-    const tokenContract = await new web3.eth.Contract(
-        TOKEN_CONTRACT_ABI,
-        TOKEN_CONTRACT_ADDR
-    );
-    const nftContractName = await nftContract.methods.name().call();
-    const tokenContractName = await tokenContract.methods.name().call();
-    console.log(nftContractName);
-    console.log(tokenContractName);
 
     const [user, created] = await User.findOrCreate({
         where: { nickname:process.env.SERVER_NICKNAME },
@@ -37,8 +17,6 @@ exports.init_server = async () => {
         }
     });
 }
-
-
 
 exports.main_get = async(req, res, next) => {
     try {
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,8 +1,18 @@
 const {User} = require('../models');
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
+// const {
+//     TOKEN_CONTRACT_ADDR,
+//     TOKEN_CONTRACT_ABI
+// } = require('../global_variables');
+
 
 exports.user_join = async(req, res, next) => {
+    // const tokenContract = await new web3.eth.Contract(
+    //     TOKEN_CONTRACT_ABI,
+    //     TOKEN_CONTRACT_ADDR,
+    // );
+
     try {
         const { email, nickname, password } = req.body;
 
@@ -20,6 +30,7 @@ exports.user_join = async(req, res, next) => {
                 res.status(409).send("이미 가입된 회원입니다.");
             } else{
                 const address = await web3.eth.personal.newAccount(password);
+                // await tokenContract.methods.approve(address, 100).call();   
 
                 User.update({address:address}, {where: {email:email}})
                 .then((result)=>{
